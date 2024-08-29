12551a29c2312ed0d3e0d0b45a9f000a92910b5d
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-08-29 13:23:54 +0300

    refactor: duplicate code in footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import { CustomLink } from "../CustomLink/CustomLink";
 import styles from "./footer.module.css";
+import { GitHubUserContainer } from "./GitHubUserContainer";
 
 export const Footer = () => {
   return (
@@ -9,39 +9,18 @@ export const Footer = () => {
         <div className={styles.footerContainer}>
           <div style={{ fontSize: "18px" }}>2024 ©</div>
           <div className={styles.githubContainer}>
-            <div className={styles.githubUserContainer}>
-              <a href="https://github.com/marakoris2022">
-                <Image
-                  src="/github.png"
-                  alt="github"
-                  width={30}
-                  height={30}
-                ></Image>
-                <span>Marakoris2022</span>
-              </a>
-            </div>
-            <div className={styles.githubUserContainer}>
-              <a href="https://github.com/LokiSpirit">
-                <Image
-                  src="/github.png"
-                  alt="github"
-                  width={30}
-                  height={30}
-                ></Image>
-                <span>LokiSpirit</span>
-              </a>
-            </div>
-            <div className={styles.githubUserContainer}>
-              <a href="https://github.com/OxygeniumO2">
-                <Image
-                  src="/github.png"
-                  alt="github"
-                  width={30}
-                  height={30}
-                ></Image>
-                <span>OxygeniumO2</span>
-              </a>
-            </div>
+            <GitHubUserContainer
+              href="https://github.com/marakoris2022"
+              userName="Marakoris2022"
+            />
+            <GitHubUserContainer
+              href="https://github.com/LokiSpirit"
+              userName="LokiSpirit"
+            />
+            <GitHubUserContainer
+              href="https://github.com/OxygeniumO2"
+              userName="OxygeniumO2"
+            />
           </div>
           <div className={styles.rsschoolLogo}>
             <a href="https://rs.school/react/">
