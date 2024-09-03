91227d20c3953a8be6e90a738e5f4eb7ccc90406
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-09-03 14:29:51 +0300

    fix: error word translation

--- app/components/REST/components/ErrorBlock.tsx
+++ app/components/REST/components/ErrorBlock.tsx
@@ -1,11 +1,14 @@
+import { useTranslations } from "next-intl";
 import styles from "./ErrorBlock.module.css";
 
 export const ErrorBlock = ({ errorText }: { errorText: string }) => {
+  const t = useTranslations("rest");
+
   if (!errorText) return <></>;
 
   return (
     <div className={styles.errorWrapper}>
-      <h1 className={styles.title}>Error:</h1>
+      <h1 className={styles.title}>{t("error")}</h1>
       <textarea className={styles.textarea} value={errorText} disabled />
     </div>
   );
