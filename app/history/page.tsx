525d637b37b7a6301336f67af666e9c185c6fb08
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-09-05 12:56:19 +0300

    feat: initial commit

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,5 +1,69 @@
-import styles from "./page.module.css";
+"use client";
+
+import { Box, Button, Container, Typography } from "@mui/material";
+import SyncAltIcon from "@mui/icons-material/SyncAlt";
+import StreamIcon from "@mui/icons-material/Stream";
+import Link from "next/link";
+
+function EmptyHistory() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        textAlign: "center",
+        padding: "30px 0px",
+      }}
+    >
+      <Typography
+        variant="h1"
+        sx={{ fontSize: { lg: "4rem", md: "3rem", xs: "2rem" }, mb: "20px" }}
+      >
+        History Page
+      </Typography>
+
+      <Typography variant="body1">
+        There is no any requests in the history.
+      </Typography>
+
+      <Typography variant="body1">
+        Please, choose a client and made your first request.
+      </Typography>
+
+      <Box
+        sx={{
+          padding: "30px 0px",
+          display: "flex",
+          flexDirection: { lg: "row", md: "row", xs: "column" },
+          justifyContent: "center",
+          gap: "20px",
+        }}
+      >
+        <Link href={"/GRAPHQL"}>
+          <Button
+            sx={{ color: "#e6009d" }}
+            variant="outlined"
+            startIcon={<StreamIcon />}
+          >
+            GraphQL Client
+          </Button>
+        </Link>
+
+        <Link href={"/GET"}>
+          <Button variant="outlined" startIcon={<SyncAltIcon />}>
+            REST Client
+          </Button>
+        </Link>
+      </Box>
+    </Box>
+  );
+}
 
 export default function History() {
-  return <div>History</div>;
+  return (
+    <Container>
+      <EmptyHistory />
+    </Container>
+  );
 }
