df5c2054a5620682933d6307fe3478821cb82e60
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-09-09 19:23:23 +0300

    feat: about project translate, styles

--- app/components/MainTemplates/components/AboutProject/AboutProject.tsx
+++ app/components/MainTemplates/components/AboutProject/AboutProject.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from 'next-intl';
 import styles from './aboutProject.module.css';
+import Image from 'next/image';
 
 export const AboutProject = () => {
   const t = useTranslations('aboutProject');
@@ -7,9 +8,43 @@ export const AboutProject = () => {
   return (
     <section className={styles.aboutProjectContainer}>
       <h3>{t('title')}</h3>
-      <p>{t('p1')}</p>
-      <p>{t('p2')}</p>
-      <p>{t('p3')}</p>
+      <div className={styles.cardContainer}>
+        <div className={styles.cardTitleContainer}>
+          <h4>
+            NextJS{' '}
+            <Image src="/nextjs-logo.svg" alt="nextjs" width={22} height={22} />{' '}
+            <span>{t('and')}</span> Firebase
+            <Image
+              src="/firebase-logo.svg"
+              alt="firebase"
+              width={22}
+              height={24}
+            />
+          </h4>
+        </div>
+        <p>{t('p1')}</p>
+      </div>
+      <div className={styles.cardContainer}>
+        <div className={styles.cardTitleContainer}>
+          <h4>{t('accessibility')}</h4>
+          <Image
+            src="/accessibility-logo.svg"
+            alt="accessibility"
+            width={22}
+            height={22}
+          />
+        </div>
+        <p>{t('p2')}</p>
+      </div>
+      <div className={styles.cardContainer}>
+        <div className={styles.cardTitleContainer}>
+          <h4>{t('history')}</h4>
+          <Image src="/history-logo.svg" alt="history" width={22} height={22} />
+        </div>
+        <p>{t('p3')}</p>
+      </div>
     </section>
   );
 };
+
+//accessibility-logo.svg
