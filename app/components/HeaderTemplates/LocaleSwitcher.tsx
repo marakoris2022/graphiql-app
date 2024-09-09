64a12f6311ab3ccb7e49fd56bdd9fe2037c14f28
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-09-09 22:40:07 +0300

    feat: fixed switcher component

--- app/components/HeaderTemplates/LocaleSwitcher.tsx
+++ app/components/HeaderTemplates/LocaleSwitcher.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { setUserLocale } from '@/services/locale';
 import { useLocale, useTranslations } from 'next-intl';
-import styles from './headerTemplates.module.css';
+import { Box, FormControl, InputLabel, NativeSelect } from '@mui/material';
 
 export default function LocaleSwitcher() {
   const t = useTranslations('localeSwitcher');
@@ -16,13 +16,32 @@ export default function LocaleSwitcher() {
   };
 
   return (
-    <select
-      className={styles.localeSwitcher}
-      defaultValue={locale}
-      onChange={handleChange}
-    >
-      <option value="en">{t('en')}</option>
-      <option value="ru">{t('ru')}</option>
-    </select>
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel variant="standard" htmlFor="uncontrolled-native">
+          {t('language')}
+        </InputLabel>
+        <NativeSelect
+          defaultValue={locale}
+          onChange={handleChange}
+          inputProps={{
+            name: 'age',
+            id: 'uncontrolled-native',
+          }}
+        >
+          <option value="en">{t('en')}</option>
+          <option value="ru">{t('ru')}</option>
+        </NativeSelect>
+      </FormControl>
+    </Box>
+
+    // <select
+    //   className={styles.localeSwitcher}
+    //   defaultValue={locale}
+    //   onChange={handleChange}
+    // >
+    //   <option value="en">{t('en')}</option>
+    //   <option value="ru">{t('ru')}</option>
+    // </select>
   );
 }
