bed133723b97bb49a8a6276fa3de4caaae113eb8
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-09-09 20:42:14 +0300

    feat: translate about us

--- app/components/MainTemplates/components/AboutUs/AboutUs.tsx
+++ app/components/MainTemplates/components/AboutUs/AboutUs.tsx
@@ -1,29 +1,32 @@
+import { useTranslations } from 'next-intl';
 import { AboutCard } from './AboutCard/AboutCard';
 import styles from './aboutUs.module.css';
 
 export const AboutUs = () => {
+  const t = useTranslations('aboutUs');
+
   return (
     <div className={styles.aboutPageWrapper}>
-      <h2 className={styles.aboutPageTitle}>About Us</h2>
+      <h2 className={styles.aboutPageTitle}>{t('title')}</h2>
       <div className={styles.aboutCardsWrapper}>
         <AboutCard
-          authorName="Olga"
-          title="Theoretical Master"
-          bio="Intelligent and purposeful, Olga is our strategic thinker. She always thoroughly understands each issue before taking action, ensuring our solutions are well-informed and effective."
+          authorName={t('olga')}
+          title={t('olgaTitle')}
+          bio={t('olgaBio')}
           git="https://github.com/lokispirit"
           photo={'/photo_olga.jpg'}
         />
         <AboutCard
-          authorName="Alexandr"
-          title="Anonymous Coder"
-          bio="Modest and persistent, Alexander is our dedicated learner. He prefers to gain knowledge through hands-on experience, bringing a practical approach to our projects."
+          authorName={t('alex')}
+          title={t('alexTitle')}
+          bio={t('alexBio')}
           git="https://github.com/oxygeniumo2"
           photo={'/photo_alex.jpg'}
         />
         <AboutCard
-          authorName="Sergey"
-          title="Not the most Important"
-          bio="Confident and grounded, Sergey values everyone's input but stands firm on his convictions. His balanced approach ensures we stay true to our vision while considering all perspectives."
+          authorName={t('sergey')}
+          title={t('sergeyTitle')}
+          bio={t('sergeyBio')}
           git="https://github.com/marakoris2022"
           photo={'/photo_sergey.jpg'}
         />
