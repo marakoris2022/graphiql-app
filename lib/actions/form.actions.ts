65c4e64f73f59cfb500138fbe62b382d1054aa08
unknown <feikomilcezzz2@gmail.com>
2024-09-22 19:29:44 +0300

    refactor: action with enum and remove else block

--- lib/actions/form.actions.ts
+++ lib/actions/form.actions.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import type { FormCheckResult } from '@/lib/types';
+import { HttpStatusCode, ResponseTitle } from '@/utils/utils';
 
 type GraphQLRequest = {
   endpointURL: string;
@@ -32,12 +33,15 @@ export async function createQuery(
   try {
     const endpointURL = data.get('endpointURL') as string | null;
     const query = data.get('query') as string | null;
+
     if (!endpointURL) {
       throw new Error('Endpoint is required');
     }
+
     if (!query) {
       throw new Error('Body of GraphQL Query is required');
     }
+
     const variables = (data.get('variables') as string | null) || '{}';
     const headersKeys = data.getAll('headerKey') as string[];
     const headersValues = data.getAll('headerValue') as string[];
@@ -54,7 +58,7 @@ export async function createQuery(
       variablesOfJSONFormat = JSON.parse(variables);
     } catch (e) {
       return {
-        title: 'Error',
+        title: ResponseTitle.ERROR,
         status: null,
         message: 'Invalid JSON format for variables.',
       };
@@ -67,38 +71,48 @@ export async function createQuery(
       variablesOfJSONFormat,
     });
 
-    if (response.status >= 400 && response.status <= 499) {
+    if (
+      response.status >= HttpStatusCode.BAD_REQUEST &&
+      response.status < HttpStatusCode.INTERNAL_SERVER_ERROR
+    ) {
       const responseOfJSONFormat = await response.json();
       return {
-        title: 'Response',
+        title: ResponseTitle.RESPONSE,
         status: response.status,
         message: `${JSON.stringify(responseOfJSONFormat, null, 2)}`,
       };
-    } else if (response.status >= 500 && response.status <= 599) {
+    }
+
+    if (response.status >= HttpStatusCode.INTERNAL_SERVER_ERROR) {
       const responseOfTextFormat = await response.text();
       return {
-        title: 'Response',
-        status: response.status,
-        message: responseOfTextFormat,
-      };
-    } else if (response.status >= 200 && response.status <= 299) {
-      const responseOfJSONFormat = await response.json();
-      return {
-        title: 'Response',
-        status: response.status,
-        message: `${JSON.stringify(responseOfJSONFormat, null, 2)}`,
-      };
-    } else {
-      const responseOfTextFormat = await response.text();
-      return {
-        title: 'Error',
+        title: ResponseTitle.RESPONSE,
         status: response.status,
         message: responseOfTextFormat,
       };
     }
+
+    if (
+      response.status >= HttpStatusCode.OK &&
+      response.status < HttpStatusCode.BAD_REQUEST
+    ) {
+      const responseOfJSONFormat = await response.json();
+      return {
+        title: ResponseTitle.RESPONSE,
+        status: response.status,
+        message: `${JSON.stringify(responseOfJSONFormat, null, 2)}`,
+      };
+    }
+
+    const responseOfTextFormat = await response.text();
+    return {
+      title: ResponseTitle.ERROR,
+      status: response.status,
+      message: responseOfTextFormat,
+    };
   } catch (error) {
     return {
-      title: 'Error',
+      title: ResponseTitle.ERROR,
       status: null,
       message: error instanceof Error ? error.message : 'Something went wrong',
     };
@@ -110,26 +124,25 @@ import { getIntrospectionQuery } from 'graphql/utilities';
 export async function createSDLQuery(
   endpointSDL: string
 ): Promise<typeof Object | null> {
-  
-    const response = await fetch(endpointSDL, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: getIntrospectionQuery(),
-      }),
-    });
+  const response = await fetch(endpointSDL, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: getIntrospectionQuery(),
+    }),
+  });
 
-    if (!response.ok) {
-      return null;
-    }
+  if (!response.ok) {
+    return null;
+  }
 
-    const result = await response.json();
+  const result = await response.json();
 
-    if (!result.data) {
-       return null;
-    }
-    return result.data;
+  if (!result.data) {
+    return null;
+  }
+  return result.data;
 }
--- utils/utils.tsx
+++ utils/utils.tsx
@@ -45,6 +45,23 @@ export enum MiddleWarePath {
   LOGOUT = '/api/logout',
 }
 
+export enum HttpStatusCode {
+  OK = 200,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 500,
+  BAD_GATEWAY = 502,
+  SERVICE_UNAVAILABLE = 503,
+  GATEWAY_TIMEOUT = 504,
+}
+
+export enum ResponseTitle {
+  RESPONSE = 'Response',
+  ERROR = 'Error',
+}
+
 export const handleScrollWithRef = (ref: React.RefObject<HTMLElement>) => {
   if (!ref.current) return;
 
