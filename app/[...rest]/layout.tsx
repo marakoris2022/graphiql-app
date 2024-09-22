5cc0fb1eda636bc1c04b8586f9f5a44951b30022
unknown <feikomilcezzz2@gmail.com>
2024-09-22 18:49:30 +0300

    fix: add type instead of define in func

--- app/[...rest]/layout.tsx
+++ app/[...rest]/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
 
+type GenerateMetadataParams = {
+  params: { locale: string };
+};
+
 export async function generateMetadata({
   params: { locale },
-}: {
-  params: { locale: string };
-}) {
+}: GenerateMetadataParams) {
   const t = await getTranslations({ locale, namespace: 'metaRest' });
 
   return {
